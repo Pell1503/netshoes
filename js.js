5b3c133b0a8a4a37bcfc9b5abfefd4499ffc7fbf
Pedro Soares <pedrosoares1503@gmail.com>
2024-07-01 17:03:18 -0300

    atualização

--- js.js
+++ js.js
@@ -1,3 +1,28 @@
 function adicionarAoCarrinho(produto) {
-    alert(produto + " foi adicionado ao carrinho!");
+    
+    
+    window.location.href = 'pagamento.html';
+}
+
+function buscarProduto() {
+    const pesquisa = document.getElementById('pesquisa').value.toLowerCase();
+    const produtos = document.querySelectorAll('.produto');
+    let encontrado = false;
+
+    produtos.forEach(produto => {
+        const nomeProduto = produto.getAttribute('data-nome').toLowerCase();
+        if (nomeProduto.includes(pesquisa)) {
+            produto.style.display = 'block';
+            encontrado = true;
+        } else {
+            produto.style.display = 'none';
+        }
+    });
+
+    const resultadoBusca = document.getElementById('resultadoBusca');
+    if (!encontrado) {
+        resultadoBusca.innerHTML = '<p>Produto não encontrado</p>';
+    } else {
+        resultadoBusca.innerHTML = '';
+    }
 }
